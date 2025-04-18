c4febc094cbda23284f68f16f354495c80d5c647
Lobo9000 <90.perk@gmail.com>
2025-04-18 21:53:30 +0200

    Delete public/src directory

--- public/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import FaktaZonen from './DollifyMe';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <DollifyMe />
-  </React.StrictMode>
-);
