2f900dbe1d92904d43204313dfcded943d4fd217
Lobo9000 <90.perk@gmail.com>
2025-04-18 21:20:13 +0200

    Create main.jsx

---
+++ public/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FaktaZonen from './FaktaZonen';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FaktaZonen />
+  </React.StrictMode>
+);
